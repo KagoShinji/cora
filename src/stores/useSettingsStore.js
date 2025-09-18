35b27aa2e3f912b7ad665d2ccadd16f8f8e48f4c
Veybengg <degraciaharvey1@gmail.com>
2025-09-18 08:28:55 +0800

    update settings

--- src/stores/useSettingsStore.js
+++ src/stores/useSettingsStore.js
@@ -1,62 +1,54 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
-import { fetchSettings, uploadLogo,changeNameAPI,changeColorAPI } from '../api/settings'
+import { fetchSettings, uploadLogo, changeNameAPI, changeColorAPI } from '../api/settings'
 
-export const useAppSettingsStore = create(
-  persist(
-    (set) => ({
-      name: "",
-      logo_path: null,
-      primary_color: null,
-      secondary_color: null,
+export const useAppSettingsStore = create((set) => ({
+  name: "",
+  logo_path: null,
+  primary_color: null,
+  secondary_color: null,
 
-      getSettings: async () => {
-        try {
-          const data = await fetchSettings()
-          set({
-            name: data.name,
-            logo_path: data.logo_path,
-            primary_color: data.primary_color,
-            secondary_color: data.secondary_color,
-          })
-        } catch (error) {
-          console.error("Failed to fetch settings:", error)
-        }
-      },
-
-      changeLogo: async (file) => {
-        try {
-          const updated = await uploadLogo(file)
-          set({ logo_path: updated.logo_path })
-        } catch (error) {
-          console.error('Failed to upload logo:', error)
-        }
-      },
-      changeName: async (newName) => {
-        set({ isLoading: true });
-        try {
-            const updated = await changeNameAPI(newName);
-            set({ name: updated.name, isLoading: false }); 
-        } catch (error) {
-            set({ error: error.message, isLoading: false });
-        }
-        },
-      changeColor: async (primaryColor, secondaryColor) => {
-        try {
-          const updated = await changeColorAPI(primaryColor, secondaryColor);
-          set({
-            primary_color: updated.primary_color,
-            secondary_color: updated.secondary_color,
-          });
-        } catch (error) {
-          console.error("Failed to change colors:", error);
-        }
-      },
-    }),
-        
-
-    {
-      name: 'app-settings-store',
+  getSettings: async () => {
+    try {
+      const data = await fetchSettings()
+      set({
+        name: data.name,
+        logo_path: data.logo_path,
+        primary_color: data.primary_color,
+        secondary_color: data.secondary_color,
+      })
+    } catch (error) {
+      console.error("Failed to fetch settings:", error)
     }
-  )
-)
+  },
+
+  changeLogo: async (file) => {
+    try {
+      const updated = await uploadLogo(file)
+      set({ logo_path: updated.logo_path })
+    } catch (error) {
+      console.error('Failed to upload logo:', error)
+    }
+  },
+
+  changeName: async (newName) => {
+    set({ isLoading: true });
+    try {
+      const updated = await changeNameAPI(newName);
+      set({ name: updated.name, isLoading: false });
+    } catch (error) {
+      set({ error: error.message, isLoading: false });
+    }
+  },
+
+  changeColor: async (primaryColor, secondaryColor) => {
+    try {
+      const updated = await changeColorAPI(primaryColor, secondaryColor);
+      set({
+        primary_color: updated.primary_color,
+        secondary_color: updated.secondary_color,
+      });
+    } catch (error) {
+      console.error("Failed to change colors:", error);
+    }
+  },
+}))
